7e59ae7e5baf542079c15d687b633e7c09d1a324
Ivan Titlov <super.titlov@inbox.ru>
2024-04-15 23:56:51 +0300

    Fixing publishing script for sql database

--- CMSys.Database/Membership/Tables/User.sql
+++ CMSys.Database/Membership/Tables/User.sql
@@ -7,9 +7,9 @@
     [FirstName] NVARCHAR(128) NOT NULL,
     [LastName] NVARCHAR(128) NOT NULL,
     [StartDate] DATE NOT NULL
-        CONSTRAINT CC_Membership_User_StartDate CHECK ([StartDate] BETWEEN '01/01/2001' AND '12/31/2100'),
+        CONSTRAINT CC_Membership_User_StartDate CHECK ([StartDate] BETWEEN '2001/01/01' AND '2100/12/31'),
     [EndDate] DATE NULL
-        CONSTRAINT CC_Membership_User_EndDate CHECK ([EndDate] BETWEEN DATEADD(day, 1, [StartDate]) AND '12/31/2100'),
+        CONSTRAINT CC_Membership_User_EndDate CHECK ([EndDate] BETWEEN DATEADD(day, 1, [StartDate]) AND '2100/12/31'),
     [Department] NVARCHAR(128) NULL,
     [Position] NVARCHAR(128) NULL,
     [Location] NVARCHAR(128) NULL,
--- CMSys.Database/Scripts/Roles.sql
+++ CMSys.Database/Scripts/Roles.sql
@@ -1,5 +1,5 @@
-﻿INSERT INTO [Membership].[Role] (Id, [Name])
+﻿INSERT INTO [Membership].[Role] ([Id], [Name])
 VALUES
     ('c4ef356c-59a6-ea11-b894-34415dd8a3d2', N'Admin'),
-    ('c5ef356c-59a6-ea11-b894-34415dd8a3d2', N'User');
+    ('c5ef356c-59a6-ea11-b894-35415dd8b5d2', N'User');
 GO
