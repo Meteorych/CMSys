d6da50810a425b3a0745d1b1212e13a480ec3661
Ivan Titlov <super.titlov@inbox.ru>
2024-04-15 22:13:45 +0300

    Changes in connection string

--- CMSysRealization/Program.cs
+++ CMSysRealization/Program.cs
@@ -1,6 +1,7 @@
 using CMSysRealization.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
 
 var builder = WebApplication.CreateBuilder(args);
 
